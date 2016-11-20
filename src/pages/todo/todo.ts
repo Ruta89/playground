a8c578a839744d45465d8b21c042a84a8fb462c8
Arek Ruta <arek.ruta89@gmail.com>
2016-11-20 18:30:48 +0100

    Todo
    Pobieranie listy z firebase i wyswietlanie w todo.html

--- src/pages/todo/todo.ts
+++ src/pages/todo/todo.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
 @Component({
   selector: 'page-todo',
   templateUrl: 'todo.html'
 })
 export class TodoPage {
+  items: FirebaseListObservable<any[]>;
 
-  constructor(public navCtrl: NavController) {}
+  constructor(public navCtrl: NavController, af: AngularFire) {
+    this.items = af.database.list('/items');
+  }
 
   ionViewDidLoad() {
     console.log('Jestes w Todo Page');

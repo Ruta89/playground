45eb5f1b65c2cb4aa00b04ba16f214f20b609500
Arek Ruta <arek.ruta89@gmail.com>
2016-11-24 16:52:55 +0100

    add message Toast and isDone update

--- src/pages/todo/todo.ts
+++ src/pages/todo/todo.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ToastController } from 'ionic-angular';
 import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
 @Component({
@@ -7,39 +7,53 @@ import { AngularFire, FirebaseListObservable } from 'angularfire2';
   templateUrl: 'todo.html'
 })
 export class TodoPage {
-  items: FirebaseListObservable<any[]>;
-  newTodo: string;
+    items: FirebaseListObservable<any[]>;
+    newTodo: string;
 
-  constructor(public navCtrl: NavController, af: AngularFire) {
-    this.items = af.database.list('/items');
-  }
+    constructor(public navCtrl: NavController, af: AngularFire, public toastCtrl: ToastController) {
+        this.items = af.database.list('/items');
+    }
 
-  addTodo = (item) => {
-    if (item) {
-      this.items.push({
-        "text": item,
-        "isDone": false
+    addTodo = (item) => {
+        if (item) {
+            this.items.push({
+                "text": item,
+                "isDone": false
+            });
+            this.showMessage('Zadanie zostalo przyjęte :) ', 'toast-success');
+        }
+    }
+
+    deleteTodo = (item) => {
+        if (item.isDone) {
+            this.items.remove(item);
+            this.showMessage('Zadanie zostalo usunięte', 'toast-delete');
+        }
+    }
+
+    //  - Czy to to samo?
+    // deleteTodo(item) {
+    //   this.items.remove(item);
+    // }
+
+    updateTodo = (key, isDone) => {
+        this.items.update(key, { isDone: isDone });
+          this.showMessage('Zadanie zostalo updateTodo', 'toast-delete');
+
+    }
+
+    public showMessage(message: string, cssClass: string ){
+       let toast = this.toastCtrl.create({
+        message: message,
+        cssClass: cssClass,
+        duration: 3000
       });
+      toast.present();
     }
-  }
 
-  deleteTodo = (item) => {
-    if (item.isDone) {
-      this.items.remove(item);
+
+    ionViewDidLoad() {
+        console.log('Jestes w Todo Page');
     }
-  }
-
-  //  - Czy to to samo?
-  // deleteTodo(item) {
-  //   this.items.remove(item);
-  // }
-
-  updateTodo = (key, isDone) => {
-    this.items.update(key, { isDone: isDone});
-  }
-
-  ionViewDidLoad() {
-    console.log('Jestes w Todo Page');
-  }
 
 }

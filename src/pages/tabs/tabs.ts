a5c0fea95ed39a60769155d0f7f99c498873867f
Arek Ruta <arek.ruta89@gmail.com>
2016-11-28 14:28:40 +0100

    tabs

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-
-
 import { HomePage } from '../home/home'
 import { TodoPage } from '../todo/todo'
 import { InwestycjePage } from '../inwestycje/inwestycje'
 import { PracaPage } from '../praca/praca'
-
+import { ProfilePage } from '../profile/profile';
+//import { NavController } from 'ionic-angular';
+//import { AuthData } from '../../providers/auth-data';
+//import { ProfileData } from '../../providers/profile-data';
+//import { AngularFire } from 'angularfire2';
 @Component({
   selector: 'page-tabs',
   templateUrl: 'tabs.html'
@@ -17,9 +18,10 @@ export class TabsPage {
   tab2Root: any = TodoPage;
   tab3Root: any = InwestycjePage;
   tab4Root: any = PracaPage;
+  tab5Root: any = ProfilePage;
 
-  constructor(public navCtrl: NavController) {
+  constructor() { }
 
-  }
+
 
 }
